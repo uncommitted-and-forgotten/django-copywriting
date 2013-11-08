4160ed4cc25f9f72feffe80f5ca6c591ee8f621d
Walter Renner <walter.renner@me.com>
2013-11-08 14:20:19 +0100

    initial commit

---
+++ copywriting/sitemaps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.contrib.sitemaps import Sitemap
+from copywriting.models import Article
+
+
+class BlogSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.5
+
+    def items(self):
+        return Article.objects.filter(status=Article.PUBLISHED).order_by('-pubDate')
+
+    def lastmod(self, obj):
+        return obj.pubDate

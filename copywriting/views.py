c45649a7c8548e84d591c73b92e966c20d1846bb
Philippe O. Wagner <wagner@arteria.ch>
2013-12-04 16:33:12 +0100

    date time threshold

--- copywriting/views.py
+++ copywriting/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+import datetime
 import urllib
 
 from django.shortcuts import render_to_response
@@ -68,7 +68,7 @@ def listArticlesByYearMonthDay(request, requestYear, requestMonth, requestDay):
 def withTag(request, in_tag):
     lTag = urllib.unquote(in_tag)
     tags = Tag.objects.filter(name=lTag)
-    articles = Article.objects.filter(tags__in=tags, status=Article.PUBLISHED, pubDate__lte=datetime.now()).order_by('-pubDate')
+    articles = Article.objects.filter(tags__in=tags, status=Article.PUBLISHED, pubDate__lte=datetime.datetime.now()).order_by('-pubDate')
 
     return render_to_response("copywriting/copywritingIndex.html", {'tag': in_tag,
                                                                     'articles': articles,

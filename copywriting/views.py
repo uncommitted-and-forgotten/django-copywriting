ce1c92595bbae89fe99ea1dcf3ef576416cde8eb
Philippe O. Wagner <wagner@arteria.ch>
2013-12-04 16:36:32 +0100

    no message

--- copywriting/views.py
+++ copywriting/views.py
@@ -68,7 +68,7 @@ def listArticlesByYearMonthDay(request, requestYear, requestMonth, requestDay):
 def withTag(request, in_tag):
     lTag = urllib.unquote(in_tag)
     tags = Tag.objects.filter(name=lTag)
-    articles = Article.objects.filter(tags__in=tags, status=Article.PUBLISHED, pubDate__lte=datetime.datetime.now()).order_by('-pubDate')
+    articles = Article.objects.filter(tags__in=tags, status=Article.PUBLISHED, pubDate__lte=datetime.datetime.now()).order_by('pubDate')
 
     return render_to_response("copywriting/copywritingIndex.html", {'tag': in_tag,
                                                                     'articles': articles,

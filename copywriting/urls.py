315a5c25429b3910446714238c28382ba727add8
Philippe O. Wagner <wagner@arteria.ch>
2013-12-04 12:07:26 +0100

    Allow slugs in url patterns

--- copywriting/urls.py
+++ copywriting/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls.defaults import *
 from .feed import blogFeed
 
 urlpatterns = patterns('copywriting',
-    (r'^feed\.rss$',                                    blogFeed()),
-    (r'^feed/$',                                        blogFeed()),
-    (r'^tag/(?P<in_tag>\w+)/$',	                        'views.withTag'),
+    (r'^feed\.rss$', blogFeed()),
+    (r'^feed/$', blogFeed()),
+    (r'^tag/(?P<in_tag>\w[\w-]+)/$', 'views.withTag'),
     # (r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', 'views.listBlogEntriesByYearMonthDay'),
-    (r'^(?P<requestYear>\d+)/(?P<requestMonth>\d+)/$',	'views.listArticlesByYearMonth'),
-    (r'^(?P<requestYear>\d+)/$',                        'views.listArticlesByYear'),
-    (r'^(?P<slug>[^\.]+)/$',                            'views.showArticle'),
-    (r'^$',                                             'views.listArticles'),
+    (r'^(?P<requestYear>\d+)/(?P<requestMonth>\d+)/$', 'views.listArticlesByYearMonth'),
+    (r'^(?P<requestYear>\d+)/$', 'views.listArticlesByYear'),
+    (r'^(?P<slug>[^\.]+)/$', 'views.showArticle'),
+    (r'^$', 'views.listArticles'),
 )

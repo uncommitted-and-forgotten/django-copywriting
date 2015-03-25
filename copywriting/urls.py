701a9fd30706771f96bb3f51e365a68f046c36b5
Philippe O. Wagner <wagner@arteria.ch>
2015-03-25 14:03:28 +0100

    Added django-compat for 1.8 support

--- copywriting/urls.py
+++ copywriting/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls.defaults import *
+from compat import patterns, url
 from .feed import blogFeed
 
 urlpatterns = patterns('copywriting',
-    (r'^feed\.rss$', blogFeed()),
-    (r'^feed/$', blogFeed()),
-    (r'^author/(?P<author>\w[^/]+)$', 'views.listArticlesByAuthor'),
-    (r'^author/(?P<author>\w[^/]+)/$', 'views.listArticlesByAuthor'),
-    (r'^tag/(?P<in_tag>\w[^/]+)$', 'views.withTag'),
-    (r'^tag/(?P<in_tag>\w[^/]+)/$', 'views.withTag'),
+    url(r'^feed\.rss$', blogFeed()),
+    url(r'^feed/$', blogFeed()),
+    url(r'^author/(?P<author>\w[^/]+)$', 'views.listArticlesByAuthor'),
+    url(r'^author/(?P<author>\w[^/]+)/$', 'views.listArticlesByAuthor'),
+    url(r'^tag/(?P<in_tag>\w[^/]+)$', 'views.withTag'),
+    url(r'^tag/(?P<in_tag>\w[^/]+)/$', 'views.withTag'),
     # (r'^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', 'views.listBlogEntriesByYearMonthDay'),
-    (r'^(?P<requestYear>\d+)/(?P<requestMonth>\d+)/$', 'views.listArticlesByYearMonth'),
-    (r'^(?P<requestYear>\d+)/$', 'views.listArticlesByYear'),
-    (r'^(?P<slug>[^\.]+)/$', 'views.showArticle'),
-    (r'^$', 'views.listArticles'),
+    url(r'^(?P<requestYear>\d+)/(?P<requestMonth>\d+)/$', 'views.listArticlesByYearMonth'),
+    url(r'^(?P<requestYear>\d+)/$', 'views.listArticlesByYear'),
+    url(r'^(?P<slug>[^\.]+)/$', 'views.showArticle'),
+    url('^$', 'views.listArticles'),
 )
